769b42c846eccdc9d173c203d15f907cb6d008e9
Mahesh512001 <chaurasiyamahesh012345@gmail.com>
2026-08-31 00:22:05 +0530

    new file:   public/hero/hero1.png

--- src/data/hero.data.ts
+++ src/data/hero.data.ts
@@ -3,17 +3,17 @@ import type { HeroSlide } from "../types/hero.types";
 export const heroSlides: HeroSlide[] = [
   {
     id: 1,
-    image: "/images/4-Tire M.S Pipe Shoes Rack ( Black ).jpg",
+    image: "/hero/hero1.png",
     alt: "Next Steel Innovation kitchen storage solutions",
   },
   {
     id: 2,
-    image: "/images/M.S Door Hanger 9 Hook.jpg",
+    image: "/hero/hero2.png",
     alt: "Next Steel Innovation kitchen organizers",
   },
   {
     id: 3,
-    image: "/images/M.S Door Hanger 5 Hook.jpg",
+    image: "/hero/hero3.png",
     alt: "Next Steel Innovation steel storage products",
   },
 ];
